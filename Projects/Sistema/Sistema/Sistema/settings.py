cb286eeba306e6aaace678c2a521e687ead167f5
nickinicole1365-cpu <nickinicole1365@gmail.com>
2026-04-29 22:39:51 -0600

    git

--- Projects/Sistema/Sistema/Sistema/settings.py
+++ Projects/Sistema/Sistema/Sistema/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'clave-local-solo-desarrollo')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
