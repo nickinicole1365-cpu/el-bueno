ab97c22650384aabe32f6f7d4f82fe03af2cc005
nickinicole1365-cpu <nickinicole1365@gmail.com>
2026-04-29 23:44:35 -0600

    git

--- Projects/Sistema/Sistema/app/urls.py
+++ Projects/Sistema/Sistema/app/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('/', views.home, name='home'),
     path('ct/', views.Create_task),
     path('delete/<int:id>/', views.delete_task, name='delete'),
     path('edit/<int:id>/', views.edit_task, name='ed'),
